1d84ccd85a51e16a2bafde7186ab54aafdce7782
M R <6248874+mrbrirup@users.noreply.github.com>
2019-05-30 02:18:49 +0100

    basic file response

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 const http2 = require('http2');
 const fs = require('fs');
+var path = require('path');
  
-function onRequest (req, resp) {
-    resp.end("Hello World");
+function onRequest (req, response) {
+    console.log('request starting...');
+
+    var filePath = 'C:\\Development\\SECDesignerPixi\\' + req.url;
+    console.log(filePath);
+    //if (filePath == './')        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 }
  
 const server = http2.createSecureServer({
