5a326be3336969ac82427db69dac1113378fd6f1
M R <6248874+mrbrirup@users.noreply.github.com>
2019-05-30 03:27:20 +0100

    update

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 const http2 = require('http2');
 const fs = require('fs');
 var path = require('path');
- 
-function onRequest (req, response) {
-    console.log('request starting...');
 
-    var filePath = process.argv[3] + req.url;
-    console.log(filePath);
-    //if (filePath == './')        filePath = './index.html';
+function onRequest(req, response) {
+  console.log('request starting...');
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
+  var filePath = process.env.argv[3] + req.url;
+  console.log(filePath);
+  //if (filePath == './')        filePath = './index.html';
+
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.wav':
+      contentType = 'audio/wav';
+      break;
+  }
+
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('./404.html', function (error, content) {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+        response.end();
+      }
     }
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
 }
- 
+   
 const server = http2.createSecureServer({
   key: fs.readFileSync('./localhost-privkey.pem'),
   cert: fs.readFileSync('./localhost-cert.pem')
 }, onRequest);
  
-server.listen(8443);
+server.listen(8443)
